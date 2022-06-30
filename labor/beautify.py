fd21ea9d461b4107c5780d4b5e8692d92804091a
Alvaro Sobral de Oliveira <alvaro.sobral.oliveira@gmail.com>
2022-06-30 16:12:24 -0300

    added total duration

--- labor/beautify.py
+++ labor/beautify.py
@@ -1,5 +1,5 @@
 import datetime
-
+from string import Formatter
 from rich.table import Table
 from rich.text import Text
 from rich.live import Live
@@ -54,6 +54,7 @@ def printTasks(tasks_tuple, projects, wage):
     
     ids = []
     wage_sum = 0
+    duration_sum = datetime.timedelta()
     for date, tasks in tasks_tuple:
         for task in tasks["tasks"]:
             if task['id'] in ids:
@@ -64,6 +65,7 @@ def printTasks(tasks_tuple, projects, wage):
             form = '%H:%M:%S'
             duration_wage = (datetime.datetime.strptime(end[11:-10],form) - datetime.datetime.strptime(start[11:-10],form)).seconds / 3600
             duration = format_to_date(end) - format_to_date(start)
+            duration_sum = duration_sum + duration
             task['cost'] = wage * float(duration_wage)
             project = match_project(task['project_id'], projects)
             table.add_row(
@@ -83,8 +85,8 @@ def printTasks(tasks_tuple, projects, wage):
         "",
         "",
         "",
-        "",
         "[green]Total:",
+        f"[green]{strfdelta(duration_sum, '{H}h {M}m')}",
         f"[green]{trunc_numbers(wage_sum)}"
     )
     return table
@@ -132,3 +134,50 @@ def print_reports(reports):
         current_month = current_month - 1
 
     return table
+
+def strfdelta(tdelta, fmt='{D:02}d {H:02}h {M:02}m {S:02}s', inputtype='timedelta'):
+    """Convert a datetime.timedelta object or a regular number to a custom-
+    formatted string, just like the stftime() method does for datetime.datetime
+    objects.
+
+    The fmt argument allows custom formatting to be specified.  Fields can 
+    include seconds, minutes, hours, days, and weeks.  Each field is optional.
+
+    Some examples:
+        '{D:02}d {H:02}h {M:02}m {S:02}s' --> '05d 08h 04m 02s' (default)
+        '{W}w {D}d {H}:{M:02}:{S:02}'     --> '4w 5d 8:04:02'
+        '{D:2}d {H:2}:{M:02}:{S:02}'      --> ' 5d  8:04:02'
+        '{H}h {S}s'                       --> '72h 800s'
+
+    The inputtype argument allows tdelta to be a regular number instead of the  
+    default, which is a datetime.timedelta object.  Valid inputtype strings: 
+        's', 'seconds', 
+        'm', 'minutes', 
+        'h', 'hours', 
+        'd', 'days', 
+        'w', 'weeks'
+    """
+
+    # Convert tdelta to integer seconds.
+    if inputtype == 'timedelta':
+        remainder = int(tdelta.total_seconds())
+    elif inputtype in ['s', 'seconds']:
+        remainder = int(tdelta)
+    elif inputtype in ['m', 'minutes']:
+        remainder = int(tdelta)*60
+    elif inputtype in ['h', 'hours']:
+        remainder = int(tdelta)*3600
+    elif inputtype in ['d', 'days']:
+        remainder = int(tdelta)*86400
+    elif inputtype in ['w', 'weeks']:
+        remainder = int(tdelta)*604800
+
+    f = Formatter()
+    desired_fields = [field_tuple[1] for field_tuple in f.parse(fmt)]
+    possible_fields = ('W', 'D', 'H', 'M', 'S')
+    constants = {'W': 604800, 'D': 86400, 'H': 3600, 'M': 60, 'S': 1}
+    values = {}
+    for field in possible_fields:
+        if field in desired_fields and field in constants:
+            values[field], remainder = divmod(remainder, constants[field])
+    return f.format(fmt, **values)
